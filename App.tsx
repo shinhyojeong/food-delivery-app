eb6505abc321ed759e1e9faaced942f096142860
shinhyojeong <shinhyojeong.w@gmail.com>
2024-06-04 22:29:51 +0900

    feat: App 코드 변경

--- App.tsx
+++ App.tsx
@@ -1,54 +1,43 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, ParamListBase} from '@react-navigation/native';
 import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
-import {Text, View, Button} from 'react-native';
+import {Text, TouchableHighlight, View} from 'react-native';
+import {useCallback} from 'react';
 
 type RootStackParamList = {
   Home: undefined;
-  Details: {itemId: number; otherParam?: string};
+  Details: undefined;
 };
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
-type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
 
 function HomeScreen({navigation}: HomeScreenProps) {
+  const onClick = useCallback(() => {
+    navigation.navigate('Details');
+  }, [navigation]);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => {
-          /* 1. Navigate to the Details route with params */
-          navigation.navigate('Details', {
-            itemId: 86,
-            otherParam: 'anything you want here',
-          });
-        }}
-      />
+      <TouchableHighlight onPress={onClick}>
+        <Text>Home Screen</Text>
+      </TouchableHighlight>
     </View>
   );
 }
 
-function DetailsScreen({route, navigation}: DetailsScreenProps) {
-  /* 2. Get the param */
-  const {itemId, otherParam} = route.params;
+function DetailsScreen({navigation}: DetailsScreenProps) {
+  const onClick = useCallback(() => {
+    navigation.navigate('Home');
+  }, [navigation]);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() =>
-          navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-          })
-        }
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <TouchableHighlight onPress={onClick}>
+        <Text>Details Screen</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -61,12 +50,11 @@ function App() {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{title: '제목'}}
+          options={{title: 'Overview'}}
         />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        {/*<Stack.Screen name="Details">*/}
-        {/*  {props => <DetailsScreen {...props} />}*/}
-        {/*</Stack.Screen>*/}
+        <Stack.Screen name="Details">
+          {props => <DetailsScreen {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
